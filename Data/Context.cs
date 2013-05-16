7b635aba0a03fd22aa11ef447bbbaa263ba35000
Nicholas Valbusa <info@squallstar.it>
2013-05-16 13:06:24 +0100

    first commit

---
+++ Data/Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Due.Data
+{
+    public class Context : DataContext
+    {
+        public Context()
+            : base("Data Source=isostore:/metroboard.sdf")
+        { }
+
+        public Table<Todo> todos;
+
+        public static Context Current
+        {
+            get
+            {
+                return (Application.Current as App).db;
+            }
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Due.Data;
+using Microsoft.Phone.Tasks;
+using Microsoft.Phone.Data.Linq;
+
+namespace Due
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private Context db;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+
+            this.db = Context.Current;
+        }
+
+        // Load data for the ViewModel Items
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var collection = (from Todo s in this.db.todos select s).ToList();
+            todayItems.ItemsSource = collection;
+        }
+
+        private void AddNew(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
