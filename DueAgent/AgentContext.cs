f0bdf0244d3091e8180ae16e3b12bdf014c78aed
Nicholas Valbusa <info@squallstar.it>
2013-08-10 01:04:48 +0100

    git include prev path

---
+++ DueAgent/AgentContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Phone.Shell;
+using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Due.Data
+{
+    public class AgentContext : DataContext
+    {
+        public AgentContext()
+            : base("Data Source=isostore:/metroboard.sdf")
+        { }
+
+        public Table<Todo> todos;
+
+        public void UpdateTileCount()
+        {
+            ShellTile appTile = ShellTile.ActiveTiles.First();
+            if (appTile != null)
+            {
+                var count = (from Todo item in todos where item.Completed == false && item.DueDate <= DateTime.Today select item).Count();
+
+                var firstItem = (from Todo s in todos where s.Completed == false && s.DueDate <= DateTime.Today orderby s.DateInsert select s).FirstOrDefault();
+
+                StandardTileData newTile = new StandardTileData
+                {
+                    Title = "Due",
+                    BackgroundImage = new Uri("Background.png", UriKind.Relative),
+                    Count = count
+                };
+
+                if (firstItem == null)
+                {
+                    newTile.BackBackgroundImage = new Uri("Background.png", UriKind.Relative);
+                    newTile.BackContent = "";
+                    newTile.BackTitle = "Nothing to do";
+                }
+                else
+                {
+                    newTile.BackBackgroundImage = new Uri("/Resources/BackBackground.png", UriKind.Relative);
+                    newTile.BackContent = firstItem.Title;
+                    newTile.BackTitle = "Due today";
+
+                    int length = 17;
+                    string title = firstItem.Title.Length > length ? firstItem.Title.Substring(0, length - 1) + "..." : firstItem.Title;
+                    newTile.Title = title;
+
+                    if (count > 1)
+                    {
+                        var secondItem = (from Todo s in todos where s.Completed == false && s.DueDate <= DateTime.Today && s.ID != firstItem.ID orderby s.DateInsert select s).FirstOrDefault();
+                        if (secondItem != null)
+                        {
+                            newTile.BackBackgroundImage = null;
+                            newTile.BackContent = firstItem.Title + "\r\n" + secondItem.Title;
+                            newTile.BackTitle = count > 2 ? "+" + (count - 2) + " more" : "Due today";
+                        }
+
+                    }
+                }
+
+                appTile.Update(newTile);
+            }
+        }
+    }
+}
---
+++ DueLibs/Todo.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.ComponentModel;
+using Microsoft.Phone.Data.Linq.Mapping;
+
+namespace Due.Data
+{
+    [Index(Columns = "DateInsert")]
+    [Table]
+    public class Todo
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true, CanBeNull = false, DbType = "INT NOT NULL Identity", AutoSync = AutoSync.OnInsert)]
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        private string title;
+
+        [Column]
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                if (title != value)
+                {
+                    NotifyPropertyChanging("Title");
+                    title = value;
+                    NotifyPropertyChanged("Title");
+                }
+            }
+        }
+
+        private DateTime _dateInsert;
+
+        [Column]
+        public DateTime DateInsert
+        {
+            get { return _dateInsert; }
+            set
+            {
+                if (_dateInsert != value)
+                {
+                    NotifyPropertyChanging("DateInsert");
+                    _dateInsert = value;
+                    NotifyPropertyChanged("DateInsert");
+                }
+            }
+        }
+
+        private DateTime _duedate;
+
+        [Column]
+        public DateTime DueDate
+        {
+            get { return _duedate; }
+            set
+            {
+                if (_duedate != value)
+                {
+                    NotifyPropertyChanging("DueDate");
+                    _duedate = value;
+                    NotifyPropertyChanged("DueDate");
+                }
+            }
+        }
+
+        private bool? _manualDueDate;
+
+        [Column(CanBeNull = true)]
+        public bool? ManualDueDate
+        {
+            get { return _manualDueDate; }
+            set
+            {
+                if (_manualDueDate != value)
+                {
+                    NotifyPropertyChanging("ManualDueDate");
+                    _manualDueDate = value;
+                    NotifyPropertyChanged("ManualDueDate");
+                }
+            }
+        }
+
+        private bool _completed;
+
+        [Column]
+        public bool Completed
+        {
+            get { return _completed; }
+            set
+            {
+                if (_completed != value)
+                {
+                    NotifyPropertyChanging("Completed");
+                    _completed = value;
+                    NotifyPropertyChanged("Completed");
+                }
+            }
+        }
+
+        public string TimeAgo
+        {
+            get
+            {
+                string x = Utilities.TimeAgo(this.DateInsert, "added").ToUpper();
+
+                if (ManualDueDate != null && ManualDueDate == true)
+                {
+                    if (this.DueDate != DateTime.Today && this.DueDate != DateTime.Today.AddDays(1))
+                    {
+                        x += ", " + Utilities.TimeInFuture(this.DueDate).ToUpper();
+                    }
+                }
+
+                return x;
+            }
+        }
+
+        public bool Overdue
+        {
+            get
+            {
+                return this.DueDate.CompareTo(DateTime.Today) <= 0;
+            }
+        }
+
+        public double UIOpacity
+        {
+            get
+            {
+                return this.Completed ? 0.25 : 1.0;
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Used to notify the page that a data context property changed
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanging Members
+
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        // Used to notify the data context that a data context property is about to change
+        private void NotifyPropertyChanging(string propertyName)
+        {
+            if (PropertyChanging != null)
+            {
+                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
